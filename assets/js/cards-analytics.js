a58c70a57fc8ff6c5c483fbfbafc7e791b713014
m85turki <m85turki@gmail.com>
2023-10-16 01:43:52 +0300

    update

--- assets/js/cards-analytics.js
+++ assets/js/cards-analytics.js
@@ -30,8 +30,8 @@
     },
     bar: {
       series1: config.colors.primary,
-      series2: '#7367F0CC',
-      series3: '#7367f099'
+      series2: '#00526BCC',
+      series3: '#00526B99'
     }
   };
 
